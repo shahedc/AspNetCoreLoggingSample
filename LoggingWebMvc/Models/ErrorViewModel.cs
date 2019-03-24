a680dcdfb5dd0794f78ff800064bc114db397405
shahedc <shahedc@gmail.com>
2019-03-24 19:41:33 -0400

    added template-generated MVC/Razor web projects

---
+++ LoggingWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoggingWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
